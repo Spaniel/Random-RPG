0cee753685d81642d6c90c71b418905841c6dea7
Spaniel <Mat_F_S@live.dk>
2013-09-19 09:32:38 +0200

    Made a utility class and two methods within it. Also tweaked the Combat class.

--- Random RPG 2013/Random RPG 2013/Combat/Combat.cs
+++ Random RPG 2013/Random RPG 2013/Combat/Combat.cs
@@ -19,50 +19,54 @@ namespace Random_RPG_2013
 
     public Character CombatStart()
     {
+      //Keeps combat going while both hero and creature is above 0 health.
       while (Hero.Health > 0 && Creature.Health > 0)
-      {
-        Console.WriteLine("Choose action:");
+        CombatPhase();
 
-        for (int i = 0; i < Hero.CharacterListOfSkills.Count(); i++)
-          Console.WriteLine("{0}. {1}", i + 1, Hero.CharacterListOfSkills[i].Name);
-
-        string _userInput = Console.ReadLine();
-        int userInput;
-        int.TryParse(_userInput, out userInput);
-
-        switch (userInput)
-        {
-          case 1:
-            DamagePhase(Hero, Creature, userInput);
-            break;
-          case 2:
-            //Incomplete.
-            DamagePhase(Hero, Creature, userInput);
-            break;
-          case 3:
-            DamagePhase(Hero, Creature, userInput);
-            break;
-          case 4:
-            DamagePhase(Hero, Creature, userInput);
-            break;
-          case 5:
-            DamagePhase(Hero, Creature, userInput);
-            break;
-        }
-
-        DamagePhase(Creature, Hero, 1);
-      }
       return Hero;
+    }
+
+    private void CombatPhase()
+    {
+      Console.WriteLine("Choose action:");
+
+      for (int i = 0; i < Hero.CharacterListOfSkills.Count(); i++)
+        Console.WriteLine("{0}. {1}", i + 1, Hero.CharacterListOfSkills[i].Name);
+
+      int userInput = Utility.ValidateUserInput(Hero.CharacterListOfSkills.Count());
+
+      switch (userInput)
+      {
+        case 1:
+          DamagePhase(Hero, Creature, userInput - 1);
+          break;
+        case 2:
+          DamagePhase(Hero, Creature, userInput - 1);
+          break;
+        case 3:
+          DamagePhase(Hero, Creature, userInput - 1);
+          break;
+        case 4:
+          DamagePhase(Hero, Creature, userInput - 1);
+          break;
+        case 5:
+          DamagePhase(Hero, Creature, userInput - 1);
+          break;
+      }
+
+      DamagePhase(Creature, Hero, 1);
     }
 
     private void DamagePhase(Character source, Character target, int skillIndex)
     {
+      //Generates a random number between 1 and damage of chosen skill.
+      int damage = Utility.GenerateRandomNumber(1, source.CharacterListOfSkills[skillIndex].Damage);
+
       //Health of target cant be reduced below zero.
-      if (source.CharacterListOfSkills[skillIndex - 1] is Skill)
-        target.Health = source.CharacterListOfSkills[skillIndex - 1].Damage < target.Health ? target.Health = target.Health - source.CharacterListOfSkills[skillIndex - 1].Damage : 0;
+      if (source.CharacterListOfSkills[skillIndex] is Skill)
+        target.Health = damage < target.Health ? target.Health = target.Health - damage : 0;
 
-      Console.WriteLine("{0} did {1} damage to {2}. {3} has {4} health left.", source.Name, source.CharacterListOfSkills[skillIndex - 1].Damage, target.Name, target.Name, target.Health);
+      Console.WriteLine("{0} did {1} damage to {2}. {3} has {4} health left.", source.Name, damage, target.Name, target.Name, target.Health);
     }
-
   }
 }
---
+++ Random RPG 2013/Random RPG 2013/Utilities/Utility.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_RPG_2013
+{
+  class Utility
+  {
+    //Could maybe be made generic, so this method could validate ints, floats, doubles etc. 
+    //Not sure how to avoid try.parsing or how to try.parse a generic type.
+    public static int ValidateUserInput(int upperLimit)
+    {
+      bool inputAccepted = false;
+      string _userInput;
+      int userInput = 0;
+
+      while (inputAccepted == false)
+      {
+        _userInput = Console.ReadLine();
+
+        if (int.TryParse(_userInput, out userInput))
+        {
+          if (userInput >= 1 && userInput <= upperLimit)
+            inputAccepted = true;
+          else
+          {
+            Console.WriteLine("Input not accepted.");
+            inputAccepted = false;
+          }
+        }
+        else
+        {
+          Console.WriteLine("Input not accepted.");
+          inputAccepted = false;
+        }
+      }
+
+      return userInput;
+    }
+
+    //Generates a random number between a given lower and upper limit.
+    public static int GenerateRandomNumber(int lowerLimit, int upperLimit)
+    {
+      Random num = new Random();
+      int randomNum = num.Next(lowerLimit, upperLimit);
+
+      return randomNum;
+    }
+  }
+}
