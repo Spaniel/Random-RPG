2e4b9608f83fb6f17f8cf4cdb5ff8bb5eb5cd74b
Spaniel <Mat_F_S@live.dk>
2013-10-01 11:50:37 +0200

    Added stats to the hero... Again.

--- Random RPG 2013/Random RPG 2013/Characters/Hero.cs
+++ Random RPG 2013/Random RPG 2013/Characters/Hero.cs
@@ -11,7 +11,20 @@ namespace Random_RPG_2013
     public List<Item> HeroListEquippedItems = new List<Item>();
     public List<Item> HeroListInventoryItems = new List<Item>();
 
-    public Hero(string name, int heroHealth, int heroDamage)
-      : base(name, heroHealth, heroDamage) {}
+    public Dictionary<string, int> HeroDictionaryStats = new Dictionary<string, int>();
+
+    public Hero(string name, int heroHealth, int heroDamage, int strength, int dexterity, int intelligence, int vitality)
+      : base(name, heroHealth, heroDamage) 
+    {
+      InitializeStats(strength, dexterity, intelligence, vitality);
+    }
+
+    private void InitializeStats(int strength, int dexterity, int intelligence, int vitality)
+    {
+      HeroDictionaryStats.Add("Strength", strength);
+      HeroDictionaryStats.Add("Dexterity", dexterity);
+      HeroDictionaryStats.Add("Intelligence", intelligence);
+      HeroDictionaryStats.Add("Vitality", vitality);
+    }
   }
 }
