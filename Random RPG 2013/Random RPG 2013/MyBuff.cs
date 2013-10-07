24b0355d5a3c4454e811878f69fa8e22b26d586a
ExpReg <madsriis92@gmail.com>
2013-10-07 20:02:35 +0200

    New buff thing

---
+++ Random RPG 2013/Random RPG 2013/MyBuff.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_RPG_2013
+{
+    class MyBuff
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public virtual void effect(Character target){} 
+    }
+
+    class DOT : MyBuff
+    {
+        int Damage { get; set; }
+
+        public DOT(int damage)
+        {
+            this.Damage = damage; 
+        }
+
+        public override void effect(Character target)
+        {
+            target.Health =- Damage; 
+        }
+    }
+}
